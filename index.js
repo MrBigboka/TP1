6160d9a902d3e0b0a0f636da4e3b575c306df106
MrBigboka <57997492+MrBigboka@users.noreply.github.com>
2021-09-22 12:18:53 -0400

    Update

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/compat/app';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//import { getDatabase } from "firebase/database";
+
 const firebaseConfig = {
+
   apiKey: "AIzaSyBgKzk3oekSRmzRwv0kqPd3KKXF8wGVuoU",
+
   authDomain: "tp1-aw.firebaseapp.com",
+
+  databaseURL: "https://tp1-aw-default-rtdb.firebaseio.com",
+
   projectId: "tp1-aw",
+
   storageBucket: "tp1-aw.appspot.com",
+
   messagingSenderId: "249938759930",
+
   appId: "1:249938759930:web:c93eb9c17a87b06e1a7943",
+
   measurementId: "G-JDE7YH2QVV"
+
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const database = getDatabase(firebaseConfig);
+
+console.log(app)
+
+/*const updatebtn = document.getElementById("updatebtn")
+const addBtn = document.getElementById("addbtn")
+const deletebtn = document.getElementById("deletebtn")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+
+addBtn.addEventListener("click", () => {
+  database.ref('/users/1/').set({
+    Email: email.value,
+    Password: password.value
+  })
+});*/
